6d794c1095c869ab923673799fedc8a675574d55
haifangqin <85476947+haifangqin@users.noreply.github.com>
2022-09-14 14:23:01 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ fi
 conda activate $NAME
 # for face recognition and alignment
 conda install -y opencv=3.4.2 
-pip install opencv-python==4.6.0.66 Pillow==8.3.1 typing-extensions==3.10.0.0 requests==2.25.1 scikit-image==0.17.2 mxnet-neuron==1.5.1.1.6.1.0 scikit-learn==0.24.2
+pip install opencv-python==4.6.0.66 Pillow==8.3.1 typing-extensions==3.10.0.0 requests==2.25.1 scikit-image==0.17.2 mxnet-neuron==1.5.1.1.6.1.0 scikit-learn==0.24.2 torch==1.11.0 easydict==1.9 torchvision==0.12.0
 # for face quality
 pip install numpy==1.17 loguru==0.6.0 tqdm==4.61.2
 
